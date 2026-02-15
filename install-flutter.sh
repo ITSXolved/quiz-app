ca0042eab6c8cddcb0ce9cdfb127bd6422376895
ITSXolved <xolvedits@gmail.com>
2026-02-15 20:21:19 +0530

    Deploy: Optimize build script for Vercel memory limits

--- install-flutter.sh
+++ install-flutter.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-# Exit on error
-set -e
+# Exit on error and print commands
+set -ex
 
-# Deployment Trigger: 2026-02-15 20:15
+# Deployment Trigger: 2026-02-15 20:25
 
-# Clone Flutter if it doesn't exist
+# Clone Flutter if it doesn't exist (Shallow clone to save space)
 if [ ! -d "flutter" ]; then
-  echo "Cloning Flutter SDK..."
-  git clone https://github.com/flutter/flutter.git -b stable flutter
+  echo "Cloning Flutter SDK (shallow)..."
+  git clone https://github.com/flutter/flutter.git -b stable --depth 1 flutter
 fi
 
 # Add Flutter to path
 export PATH="$PATH:`pwd`/flutter/bin"
+
+# Disable analytics to save resources
+flutter config --no-analytics
 
 # Precache web artifacts
 echo "Downloading web artifacts..."
@@ -22,8 +25,8 @@ flutter precache --web
 echo "Resolving dependencies..."
 flutter pub get
 
-# Build for web
+# Build for web using HTML renderer (saves memory vs CanvasKit)
 echo "Building for web..."
-flutter build web --release --no-tree-shake-icons
+flutter build web --release --web-renderer html --no-tree-shake-icons
 
 echo "Build complete!"
