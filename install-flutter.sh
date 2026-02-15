1753c9275f02d8bc803a99106c0430aa709334aa
ITSXolved <xolvedits@gmail.com>
2026-02-15 23:39:29 +0530

    chore: Add install-flutter.sh for Vercel deployment

---
+++ install-flutter.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Clone Flutter SDK
+if [ -d "flutter" ]; then
+    cd flutter
+    git pull
+    cd ..
+else
+    git clone https://github.com/flutter/flutter.git -b stable --depth 1
+fi
+
+# Add Flutter to PATH
+export PATH="$PATH:`pwd`/flutter/bin"
+
+# Run Flutter doctor to download dependencies
+flutter doctor -v
